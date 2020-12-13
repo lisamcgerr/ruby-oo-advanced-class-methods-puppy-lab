6858efbf4265c895d17fb8c4aa944738ad23bdde
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2020-12-13 17:36:05 -0600

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,34 @@
-# Add your code here
+require 'pry'
+class Dog
+    attr_reader :name
+
+    @@all = [ ]
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        @@all.each {|dog| puts dog.name}
+    end
+
+    def save
+        @@all << self
+    end
+
+end
+
+Dexter = Dog.new("Dexter")
+Mila = Dog.new("Mila")
+Kota = Dog.new("Kota")
+Maya = Dog.new("Maya")
+#binding.pry
+
